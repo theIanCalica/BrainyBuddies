635b7027716a4566116baa730939ed40af05983d
Ian Calica <Iggc654@gmail.com>
2024-10-24 18:30:51 +0800

    Added footer

---
+++ resources/views/Layout/footer.blade.php
@@ -0,0 +1,43 @@
+<footer class="bg-dark text-white pt-2 pb-2">
+    <div class="row">
+        <!-- About Section -->
+        <div class="col-md-4">
+            <h5 class="small">About BrainyBuddies</h5>
+            <p class="small">Interactive quizzes and resources to help children learn in a fun way.</p>
+        </div>
+
+        <div class="col-md-4">
+            <h5 class="small">About Us</h5>
+            <ul class="list-unstyled">
+                <li><a href="/" class="text-white small">About BrainyBuddies</a></li>
+                <li><a href="/quizzes" class="text-white small">How it works</a></li>
+            </ul>
+        </div>
+        <!-- Quick Links -->
+        <div class="col-md-4">
+            <h5 class="small">Quick Links</h5>
+            <ul class="list-unstyled">
+                <li><a href="/" class="text-white small">Home</a></li>
+                <li><a href="/quizzes" class="text-white small">Quizzes</a></li>
+                <li><a href="/about" class="text-white small">About Us</a></li>
+                <li><a href="/contact" class="text-white small">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- Social Media Links -->
+    <div class="row mt-2">
+        <div class="col-md-12 text-center">
+            <a href="#" class="text-white me-3 small"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="text-white me-3 small"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="text-white me-3 small"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+
+    <!-- Footer Bottom -->
+    <div class="row mt-2">
+        <div class="col-md-12 text-center">
+            <p class="small">&copy; 2024 BrainyBuddies. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('Layout.app')
+
+@section('content')
+    <h1>Hi</h1>
+@endsection
